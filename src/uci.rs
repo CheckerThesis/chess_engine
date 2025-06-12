b0a0fdcec38909198a4222445fa1144d900d1abc
CheckerThesis <tiencao@fastmail.com>
2025-06-12 11:08:19 -0500

    Refactor uci protocol. Add uci test.

--- src/uci.rs
+++ src/uci.rs
@@ -1,6 +1,8 @@
+use colored::Colorize;
+
 use crate::{
     attack::square_attacked, board::parse_fen, defs::{
-        extract_movelist_move, Board, MoveList, SearchInfo, BLACK, ENGINE_OPTIONS, HASH_TABLE, MAX_DEPTH, MAX_THREADS, WHITE
+        extract_movelist_move, Board, MoveList, SearchInfo, UciCommand, BLACK, ENGINE_OPTIONS, HASH_TABLE, MAX_DEPTH, MAX_THREADS, WHITE
     }, io::{parse_move, print_move}, makemove::{make_move, take_move}, movegen::generate_all_moves, perft::perft_test, pvtable::clear_hash_table, search::search_position, FEN_START
 };
 use std::{
@@ -12,11 +14,7 @@ use std::{
 };
 
 // go depth 6 wtime 1000 btime 1000 binc 1000 winc 1000 movetime 1000 movestogo 40
-pub fn parse_go(
-    input: &String,
-    info: Arc<SearchInfo>,
-    position: &mut Board,
-) -> Option<JoinHandle<()>> {
+fn parse_go(input: &String, info: Arc<SearchInfo>, position: &mut Board) -> Option<JoinHandle<()>> {
     let tokens: Vec<&str> = input.split_whitespace().collect();
     let mut i = 0;
 
@@ -115,10 +113,7 @@ pub fn parse_go(
     Some(main_search_thread)
 }
 
-// position fen fenstr
-// position startpos
-// .. moves e2e4 e7e5 ..
-pub fn parse_position(input: &String, position: &mut Board) {
+fn parse_position(input: &String, position: &mut Board) {
     let mut moves_index: usize = input.len();
     if let Some(move_index) = input.find("moves") {
         moves_index = move_index;
@@ -152,6 +147,163 @@ pub fn parse_position(input: &String, position: &mut Board) {
 }
 
 pub fn uci_loop() {
+    let mut main_search_thread: Option<JoinHandle<()>> = None;
+    let position: &mut Board = &mut Board::default();
+    let info: Arc<SearchInfo> = SearchInfo::new();
+    info.set_thread_num(1);
+
+    print_uci_id();
+
+    while handle_uci_command(String::from(""), &mut main_search_thread, position, &info) {}
+
+    if let Some(thread) = main_search_thread.take() {
+        match thread.join() {
+            // Ok(_) => println!("Thread joined successfully."),
+            Ok(_) => print!(""),
+            Err(e) => eprintln!("Error joining thread: {:?}", e),
+        }
+    }
+}
+
+fn handle_uci_command(input: String, main_search_thread: &mut Option<JoinHandle<()>>, position: &mut Board, info: &Arc<SearchInfo>) -> bool {
+    let mut user_input = String::new();
+
+    if input == "" { io::stdin().lock().read_line(&mut user_input).unwrap(); }
+    else { user_input = input; }
+
+    user_input = user_input.trim().to_string();
+
+    match parse_uci_command(&user_input) {
+        UciCommand::Uci => print_uci_id(),
+        UciCommand::IsReady => println!("readyok"),
+        UciCommand::SetOption(_) => process_setoption(&user_input, info),
+        UciCommand::UciNewGame => {
+            clear_hash_table(&HASH_TABLE);
+            parse_position(&"position startpos".to_string(), position);
+        }
+        UciCommand::Position(_) => parse_position(&user_input, position),
+        UciCommand::Go(_) => *main_search_thread = parse_go(&user_input, Arc::clone(info), position),
+        UciCommand::Stop => {
+            info.stopped.store(true, Ordering::Relaxed);
+            if let Some(thread) = main_search_thread.take() {
+                if let Err(e) = thread.join() {
+                    eprintln!("Error joining thread: {:?}", e);
+                }
+            }
+        }
+        UciCommand::Quit => {
+            info.stopped.store(true, Ordering::Relaxed);
+            return false;
+        }
+        UciCommand::Testing(_) => testing(&user_input, main_search_thread, position, info),
+        UciCommand::Generate => generate(position),
+        _ => {}
+    }
+    true
+}
+fn parse_uci_command(input: &str) -> UciCommand {
+    let tokens: Vec<&str> = input.trim().split_whitespace().collect();
+
+    if tokens.is_empty() { return UciCommand::Unknown }
+
+    return match tokens[0] {
+        "uci" => UciCommand::Uci,
+        "isready" => UciCommand::IsReady,
+        "setoption" => UciCommand::SetOption(input.to_string()),
+        "ucinewgame" => UciCommand::UciNewGame,
+        "position" => UciCommand::Position(input.to_string()),
+        "go" => UciCommand::Go(input.to_string()),
+        "stop" => UciCommand::Stop,
+        "quit" => UciCommand::Quit,
+        "testing" => UciCommand::Testing(String::from(input)),
+        "generate" => UciCommand::Generate,
+        _ => UciCommand::Unknown,
+    }
+}
+
+fn print_uci_id() {
+    let name = "Vault";
+    let author = "Tein Cow";
+    println!("id name {}\nid author {}\nuciok", name, author);
+}
+
+// TODO opening book
+fn process_setoption(input: &str, info: &SearchInfo) {
+    let tokens: Vec<&str> = input.trim().split_whitespace().collect();
+
+    match tokens[2] {
+        "Threads" => {
+            info.set_thread_num(tokens[4].parse::<u8>().unwrap());
+            // println!("threads count {}", info.get_thread_num());
+        },
+        _ => print!("")
+    }
+}
+
+fn generate(position: &mut Board) {
+    println!("movelist start");
+    let mut move_list = MoveList::default();
+    generate_all_moves(position, &mut move_list);
+
+    let mut move_list_legal = Vec::with_capacity(move_list.count);
+    let mut in_check = false;
+
+    for mv in move_list.moves.iter().take(move_list.count) {
+        let za_move = extract_movelist_move(*mv);
+        let made_move = make_move(position, za_move);
+
+        if made_move { // add check indicator to movelist output
+            move_list_legal.push(print_move(za_move));
+            take_move(position);
+            // 4375 3095
+        } else if square_attacked(position.king_square[position.side as usize] as usize, (position.side ^ 1) as usize, position) {
+            // println!("position.king_square: {}    position.side: {}", position.king_square[position.side as usize], position.side);
+            // print_board(position);
+            // println!("{}    {}", za_move, print_move(za_move));
+            in_check = true;
+        }
+    }
+
+    for mv in move_list_legal {
+        println!("{}", mv);
+    }
+    println!("movelist end {}", in_check);
+}
+
+fn testing(input: &str, main_search_thread: &mut Option<JoinHandle<()>>, position: &mut Board, info: &Arc<SearchInfo>) {
+    let tokens: Vec<&str> = input.trim().split_whitespace().collect();
+
+    match tokens[1] {
+        "perft" => {
+            let depth: Vec<&str> = input.split_whitespace().collect();
+            perft_test(depth[1].parse::<u8>().unwrap(), position);
+        },
+        "uci" => uci_test(main_search_thread, position, info),
+        _ => print!("")
+    }
+}
+fn uci_test(main_search_thread: &mut Option<JoinHandle<()>>, position: &mut Board, info: &Arc<SearchInfo>) {
+    let inputs = [
+        String::from("position fen r2qkbnr/1b5p/p1n2p2/1pN1pNp1/1P1pP3/1Q4P1/PBPPBP1P/R3K2R w KQkq g6 0 1"),
+        String::from("go depth 5"),
+        String::from("go movetime 5000"),
+        String::from("position fen r5k1/pppn2p1/3p4/4pq1n/1PPb3K/P2P1r2/8/1N6 w - - 3 56"),
+        String::from("go wtime 15000 btime 15000 winc 1000 binc 1000"),
+        String::from("go movetime 20000"),
+        String::from("stop")
+    ];
+    let times = [0.5, 5.0, 5.0, 0.5, 4.0, 3.0, 0.0];
+
+    for i in 0..inputs.len() {
+        println!("{}", format!("{}", inputs[i].bright_magenta()));
+        // println!("Sending: {}", inputs[i]);
+        handle_uci_command(inputs[i].clone(), main_search_thread, position, info);
+
+        thread::sleep(Duration::from_secs_f32(times[i]));
+    }
+}
+
+fn uci_loo2p() {
     let name = "Vault";
     let mut testing = false;
 
@@ -160,7 +312,7 @@ pub fn uci_loop() {
     let mut main_search_thread: Option<JoinHandle<()>> = None;
     let position: &mut Board = &mut Board::default();
     let info = SearchInfo::new();
-    info.set_thread_num(4);
+    info.set_thread_num(2);
     println!("thread count {}", info.get_thread_num());
 
     println!("id name {}", name.to_string());
@@ -190,6 +342,7 @@ pub fn uci_loop() {
         } else {
             io::stdin().lock().read_line(&mut user_input).unwrap();
         }
+        
         user_input = user_input.trim().to_string();
 
         if user_input == "" {
@@ -211,7 +364,8 @@ pub fn uci_loop() {
 
             if let Some(thread) = main_search_thread.take() {
                 match thread.join() {
-                    Ok(_) => println!("Thread joined successfully."),
+                    // Ok(_) => println!("Thread joined successfully."),
+                    Ok(_) => print!(""),
                     Err(e) => eprintln!("Error joining thread: {:?}", e),
                 }
             } else {
@@ -272,25 +426,8 @@ pub fn uci_loop() {
                 println!("{}", mv);
             }
             println!("movelist end {}", in_check);
-
-            // let side = position.side;
-
-            // position.side = WHITE as u8;
-            // print!("{} ", evaluate_position(position));
-
-            // position.side = BLACK as u8;
-            // println!("{}", evaluate_position(position));
-
-            // position.side = side;
         } else if user_input.contains("data") {
             position.print_data();
-        }
-    }
-
-    if let Some(thread) = main_search_thread.take() {
-        match thread.join() {
-            Ok(_) => println!("Thread joined successfully."),
-            Err(e) => eprintln!("Error joining thread: {:?}", e),
         }
     }
 }
